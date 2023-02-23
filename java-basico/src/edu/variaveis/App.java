c3e4354d84b514b86897b85f4c118072583ae5e1
Diego Hernandes <diegowalhernandes@gmail.com>
2023-02-23 11:17:04 -0300

    M

--- java-basico/src/edu/variaveis/App.java
+++ java-basico/src/edu/variaveis/App.java
@@ -1,8 +1,7 @@
 package edu.variaveis;
 
-public class TiposVariaveis {
-
+public class App {
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500.;
+        System.out.println("Hello, World!");
     }
 }
