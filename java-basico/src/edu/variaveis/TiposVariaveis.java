bdb4f08bd5822f1121d49401e1fbfdf8a46a1313
Diego Hernandes <diegowalhernandes@gmail.com>
2023-02-23 11:28:59 -0300

    m

--- java-basico/src/edu/variaveis/TiposVariaveis.java
+++ java-basico/src/edu/variaveis/TiposVariaveis.java
@@ -3,6 +3,13 @@ package edu.variaveis;
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
-        double salarioMinimo = 2500.;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 2107033; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // tipo long precisa terminar com um L l 
+        float pi = 3.14F; // tipo float precisa terminar com um f ou F
+        double salario = 1275.33;
+
     }
 }
+
