23ada0fdd0ac860babf5841cd76b6dc61a3fe168
Diego Hernandes <diegowalhernandes@gmail.com>
2023-02-23 13:49:55 -0300

    m

--- java-basico/src/edu/variaveis/TiposVariaveis.java
+++ java-basico/src/edu/variaveis/TiposVariaveis.java
@@ -10,6 +10,23 @@ public class TiposVariaveis {
         float pi = 3.14F; // tipo float precisa terminar com um f ou F
         double salario = 1275.33;
 
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.print(ano);
+        System.out.print(idade);
+        //valor_nunca_podera_mudar
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+        System.out.print(VALOR_DE_PI);
     }
 }
-
